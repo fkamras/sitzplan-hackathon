8516dabef306cfb52a3679350085ddbd86af644a
Roger Braun <roger@rogerbraun.net>
2016-09-21 13:53:00 +0200

    Get map working again.

--- app/game-map.js
+++ app/game-map.js
@@ -3,110 +3,64 @@ import tiled from './lib/tiled.js';
 import pixi from 'pixi.js';
 import _ from 'lodash';
 
+const desert = JSON.parse(require('raw!./example/desert.json'));
+const desertTileset = require('xml!./example/desert.tsx');
+const image = '/' + require('file!./example/tmw_desert_spacing.png');
+
+import textureBuilder from './lib/texture-builder.js';
+import stageBuilder from './lib/stage-builder.js';
+
+const tilesetsMap = {
+  'desert.tsx': desertTileset
+};
+
+const tilesetImagesMap = {
+  'tmw_desert_spacing.png': image
+};
+
+const stage = new pixi.Container();
+
+const desertParsed = tiled.parse(desert);
+const loader = pixi.loader
+        .add(image);
+
+const container = document.getElementById('map-container');
+const parkingSpace = document.getElementById('map-parking-space');
+
+const startRendering = () => {
+
+  const renderer = pixi.autoDetectRenderer(2000, 2000);
+
+  const setup = () => {
+    const dict = textureBuilder.buildTilesetDict(desertParsed.tilesets, tilesetsMap, tilesetImagesMap);
+    const map = stageBuilder.buildStage(desertParsed, dict);
+
+    stage.addChild(map);
+
+    container.appendChild(renderer.view);
+    renderer.render(stage);
+  };
+
+  loader.load(setup);
+};
+
+startRendering();
 
 export const GameMap = React.createClass({
 
   componentDidMount() {
+    const internalContainer = this.refs.container;
+    internalContainer.appendChild(container);
+  },
 
-    const desert = JSON.parse(require('raw!./example/desert.json'));
-    const desertTileset = require('xml!./example/desert.tsx');
-    const image = require('file!./example/tmw_desert_spacing.png');
-
-    const tilesetsMap = {
-      'desert.tsx': desertTileset
-    };
-
-    const tilesetImagesMap = {
-      'tmw_desert_spacing.png': image
-    };
-
-    const container = document.getElementsByClassName('container')[0];
-
-    const renderer = pixi.autoDetectRenderer(2000, 2000);
-
-    const stage = new pixi.Container();
-
-
-    const desertParsed = tiled.parse(desert);
-
-    const background = new pixi.Container();
-
-    const fillBackground = (tilemap, textureDict, container) => {
-      const fillLayer = (layer) => {
-        _.times(tilemap.height, (y) => {
-          _.times(tilemap.width, (x) => {
-            const tilePosition = x + tilemap.width * y;
-
-            const tileNumber = layer.data[tilePosition];
-
-            const texture = textureDict[tileNumber];
-            const sprite = new pixi.Sprite(texture);
-
-            sprite.x = x * tilemap.tilewidth;
-            sprite.y = y * tilemap.tileheight;
-
-            container.addChild(sprite);
-          });
-        });
-      };
-
-      _.each(tilemap.layers, fillLayer);
-    };
-
-    const buildTilesetDict = (tilesets) => {
-      const dict = {};
-
-      _.each(tilesets, (tileset) => {
-        const realTileset = tilesetsMap[tileset.source];
-        const realImage = tilesetImagesMap[realTileset.tileset.image[0].$.source];
-        const tileWidth = _.parseInt(realTileset.tileset.$.tilewidth);
-        const imageWidth = _.parseInt(realTileset.tileset.image[0].$.width);
-        const margin = _.parseInt(realTileset.tileset.$.margin);
-        const spacing = _.parseInt(realTileset.tileset.$.spacing);
-        const imagesPerRow = (imageWidth - ((2 * margin) - spacing)) / (tileWidth + spacing);
-
-        const textureForTile = (tileNumber) => {
-          const xPos = tileNumber % imagesPerRow;
-          const yPos = (tileNumber - xPos) / imagesPerRow;
-
-          const rect = new pixi.Rectangle(1 + (xPos * tileWidth) + xPos, 1 + (yPos * tileWidth) + yPos, tileWidth, tileWidth);
-          const texture = new pixi.Texture(pixi.utils.TextureCache[realImage], rect);
-
-          return texture;
-        };
-
-        _.times(realTileset.tileset.tile.length, (i) => {
-          const texture = textureForTile(i);
-          dict[tileset.firstgid + i] = texture;
-        });
-      });
-
-      return dict;
-    };
-
-    const setup = () => {
-      const dict = buildTilesetDict(desertParsed.tilesets);
-      fillBackground(desertParsed, dict, background);
-
-      stage.addChild(background);
-
-      container.appendChild(renderer.view);
-      renderer.render(stage);
-    };
-
-    pixi.loader
-      .add(image)
-    .load(setup);
-
-
+  componentWillUnmount() {
+    parkingSpace.appendChild(container);
   },
 
   render() {
-
     return (
-        <div className="container"></div>
-      )
-
+        <div ref='container' className="container"></div>
+    );
   }
 
-})
+});
