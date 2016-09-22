30fe72ed4507e3f18735750062e6017fc7047c83
Alexander Gitter <agitter@applause.com>
2016-09-22 12:21:39 +0200

    Remove unused socket

--- server/lib/sitzplan/endpoint.ex
+++ server/lib/sitzplan/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule Sitzplan.Endpoint do
   use Phoenix.Endpoint, otp_app: :sitzplan
 
-  socket "/socket", Sitzplan.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
